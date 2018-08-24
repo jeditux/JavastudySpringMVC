7c0e486cc1f859302f19bffd7ca355366ecd5805
Anatoly Gostev <agostev@siliconrussia.com>
2018-08-24 09:03:12 +0300

    Uploading files to server; creating excel and pdf documents

---
+++ src/main/java/ru/jeditux/JavastudyMVCDemo/mvc/excelpdf/Cat.java
@@ -0,0 +1,25 @@
+package ru.jeditux.JavastudyMVCDemo.mvc.excelpdf;
+
+public class Cat {
+    private String name;
+    private String color;
+    private int weight;
+
+    public Cat(String name, String color, int weight) {
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
